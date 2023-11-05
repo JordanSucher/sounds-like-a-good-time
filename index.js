27812d944894eb23de535804aa38fe49701962e8
JordanSucher <jsucher@gmail.com>
2023-11-05 18:13:17 -0500

    refreshing lfs

--- index.js
+++
@@ -1,9 +0,0 @@
-import App from './App.js'
-import React from 'react';
-import { createRoot } from "react-dom/client";
-const root = createRoot(document.getElementById("app"));
-
-root.render (
-    <App />
-)
-
