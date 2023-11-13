4c6aa2f857f66d812613939943b5b98d73413695
JordanSucher <jsucher@gmail.com>
2023-11-13 16:43:16 -0500

    more polyfill goodness

--- Client/index.js
+++ Client/index.js
@@ -2,6 +2,8 @@ import Navbar from './Navbar.js';
 import RouteObj from './Router.js';
 import React from 'react';
 import { createRoot } from "react-dom/client";
+import {Buffer} from 'buffer';
+
 
 const root = createRoot(document.getElementById("app"));
 
